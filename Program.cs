10ba81e8722c28d0742f99d6af745c0e7fe1b8d3
axisfold <56945516+axisfold@users.noreply.github.com>
2019-10-29 14:10:44 +0800

    Tried adding in libjpeg-turbo support but it doesn't work yet

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 ﻿using CommandLine;
 using SkiaSharp;
 using System;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
+using TurboJpegWrapper;
 
 namespace ImageConvertCore
 {
@@ -20,6 +23,9 @@ namespace ImageConvertCore
 
             [Option("jpg-quality", Required = false, HelpText = "Set quality level for JPG output.", Default = 72)]
             public int JpgLevel { get; set; }
+
+            [Option("turbo", Required = false, HelpText = "Use libjpeg-turbo", Default = false)]
+            public bool Turbo { get; set; }
         }
 
         static void Main(string[] args)
@@ -28,6 +34,7 @@ namespace ImageConvertCore
             var destFileName = "";
             var pngLevel = 1;
             var jpgQuality = 72;
+            var turbo = false;
             Parser.Default.ParseArguments<Options>(args)
                    .WithParsed<Options>(o =>
                    {
@@ -35,9 +42,24 @@ namespace ImageConvertCore
                        destFileName = o.DestinationFileName;
                        pngLevel = o.PngLevel;
                        jpgQuality = o.JpgLevel;
+                       turbo = o.Turbo;
                    });
 
             if (sourceFileName == null || destFileName == null) return;
+
+            if (turbo && (destFileName.ToLower().EndsWith("jpg") || destFileName.ToLower().EndsWith("jpeg")))
+            {
+                var bmp = (Bitmap)Image.FromFile(sourceFileName);
+                var data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadOnly,
+                        bmp.PixelFormat);
+                var _compressor = new TJCompressor();
+                var options = TJSubsamplingOptions.TJSAMP_420;
+                var result = _compressor.Compress(data.Scan0, data.Stride, data.Width, data.Height, ConvertPixelFormat(data.PixelFormat), options, jpgQuality, TJFlags.NONE);
+
+                File.WriteAllBytes(destFileName, result);
+
+                return;
+            }
 
             using (var input = File.OpenRead(sourceFileName))
             {
@@ -49,11 +71,12 @@ namespace ImageConvertCore
                     {
                         if (destFileName.ToLower().EndsWith("png"))
                         {
-                            sourceBitmap.PeekPixels().Encode(new SKPngEncoderOptions {
+                            sourceBitmap.PeekPixels().Encode(new SKPngEncoderOptions
+                            {
                                 ZLibLevel = pngLevel,
                                 FilterFlags = SKPngEncoderFilterFlags.Paeth
                             }).AsStream().CopyTo(output);
-                            
+
                         }
                         else if (destFileName.ToLower().EndsWith("jpg") || destFileName.ToLower().EndsWith("jpeg"))
                         {
@@ -64,6 +87,22 @@ namespace ImageConvertCore
                         }
                     }
                 }
+            }
+        }
+
+        public static TJPixelFormats ConvertPixelFormat(PixelFormat pixelFormat)
+        {
+            switch (pixelFormat)
+            {
+                case PixelFormat.Format32bppArgb:
+                case PixelFormat.Format32bppPArgb:
+                    return TJPixelFormats.TJPF_BGRA;
+                case PixelFormat.Format24bppRgb:
+                    return TJPixelFormats.TJPF_BGR;
+                case PixelFormat.Format8bppIndexed:
+                    return TJPixelFormats.TJPF_GRAY;
+                default:
+                    throw new NotSupportedException(string.Format("Provided pixel format \"{0}\" is not supported", pixelFormat));
             }
         }
     }
